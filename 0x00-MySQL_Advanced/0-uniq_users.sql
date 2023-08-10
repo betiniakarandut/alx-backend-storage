60ee0fe1930037f339bba4db20c8e3b9d009ceb7
betiniakarandut <betiniakarandut@gmail.com>
2023-08-10 03:15:04 +0000

    New:database

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+--Creates holberton db
+CREATE DATABASE holberton;
 --Creates a table called users
 IF NOT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = 'holberton' AND table_name = 'users') THEN
 	CREATE TABLE users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
