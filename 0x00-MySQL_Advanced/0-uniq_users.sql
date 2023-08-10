927e3fd0f4bcacc99e52793daff0da5948643658
betiniakarandut <betiniakarandut@gmail.com>
2023-08-10 15:04:59 +0100

    Inpection: code review

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,7 @@
 -- Checks if table already created
-IF NOT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = 'holberton' AND table_name = 'users') THEN
-    --Create table if doesnt exist
-    CREATE TABLE users (
-	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-	`email` VARCHAR (255) NOT NULL UNIQUE, 
-	`name` VARCHAR(255)
-    ) ENGINE=InnoDB;
-END IF;
+--Create table if doesnt exist
+CREATE TABLE IF NOT EXISTS users (
+    `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+    `email` VARCHAR (255) NOT NULL UNIQUE, 
+    `name` VARCHAR(255)
+) ENGINE=InnoDB;
