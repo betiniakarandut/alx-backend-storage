5c88d3d49b6cced32bdba6220c07caa6192d9c36
betiniakarandut <betiniakarandut@gmail.com>
2023-08-10 12:42:03 +0000

    Create: users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --Drop table if already created
 DROP TABLE IF EXISTS users;
 --Create table
-CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
                    email VARCHAR (255) NOT NULL UNIQUE, 
                    name VARCHAR(255)
                   );
