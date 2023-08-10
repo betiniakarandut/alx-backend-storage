455020585da54e70d7c1bde3eeee2df3de292474
betiniakarandut <betiniakarandut@gmail.com>
2023-08-10 02:39:59 +0000

    Breakdown: code

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
--- 0-uniq_users.sql task
-
 --Creates database holberton
 CREATE DATABASE holberton;
 
 --Creates a table called users
-CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR (255) NOT NULL UNIQUE, name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                                 email VARCHAR (255) NOT NULL UNIQUE, 
+                                 name VARCHAR(255)
+                                );
