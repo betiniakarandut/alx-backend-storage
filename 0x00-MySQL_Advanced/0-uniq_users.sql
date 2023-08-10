e33422ab4348e30cbf44e2db16a60db13b04c72d
betiniakarandut <betiniakarandut@gmail.com>
2023-08-10 15:07:16 +0100

    Inpection: code review

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,5 @@
 --Create table if doesnt exist
 CREATE TABLE IF NOT EXISTS users (
     `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-    `email` VARCHAR (255) NOT NULL UNIQUE, 
-    `name` VARCHAR(255)
+    `email` VARCHAR (255) NOT NULL UNIQUE
 ) ENGINE=InnoDB;
