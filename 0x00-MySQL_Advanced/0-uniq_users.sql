6183d9e7002b5b93f2ef7a7325895261f13f7fed
betiniakarandut <betiniakarandut@gmail.com>
2023-08-10 03:10:02 +0000

    Condition:check

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
 --Creates a table called users
-CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-                                 email VARCHAR (255) NOT NULL UNIQUE, 
-                                 name VARCHAR(255)
-                                );
+IF NOT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = 'holberton' AND table_name = 'users') THEN
+	CREATE TABLE users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                           email VARCHAR (255) NOT NULL UNIQUE, 
+                           name VARCHAR(255)
+                          );
