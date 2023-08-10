f636182da73df12c0b54fc95d90c81b7ea919088
betiniakarandut <betiniakarandut@gmail.com>
2023-08-10 13:26:11 +0000

    Update: users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,8 +2,8 @@
 IF NOT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = 'holberton' AND table_name = 'users') THEN
     --Create table if doesnt exist
     CREATE TABLE users (
-	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-	email VARCHAR (255) NOT NULL UNIQUE, 
-	name VARCHAR(255)
-    );
+	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+	`email` VARCHAR (255) NOT NULL UNIQUE, 
+	`name` VARCHAR(255)
+    ) ENGINE=InnoDB;
 END IF;
