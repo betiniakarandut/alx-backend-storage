a44f54d9952deb3c07c7eb62334d643aa6ecf136
betiniakarandut <betiniakarandut@gmail.com>
2023-08-10 13:04:58 +0000

    Update: users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,9 @@
---Drop table if already created
-DROP TABLE IF EXISTS users;
---Create table
-CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-                   email VARCHAR (255) NOT NULL UNIQUE, 
-                   name VARCHAR(255)
-                  );
+-- Checks if table already created
+IF NOT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = 'holberton' AND table_name = 'users') THEN
+    --Create table if doesnt exist
+    CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+	email VARCHAR (255) NOT NULL UNIQUE, 
+	name VARCHAR(255)
+    );
+END IF;
