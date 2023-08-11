e15db6a0be2577b9ad2bd518a30dc415786df6ea
betiniakarandut <betiniakarandut@gmail.com>
2023-08-11 02:06:16 +0100

    Check:Did I passed

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,4 @@
--- Checks if table already created
---Create table if doesnt exist
+--Creates table users
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
