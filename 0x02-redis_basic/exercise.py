80fd8465410f1779c369d1ec7df5fcb9b894d3b5
betiniakarandut <betiniakarandut@gmail.com>
2023-08-17 02:33:46 +0000

    Annotation: Union[str, bytes, int, float]

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
-        key: str = str(uuid.uuid4())
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key: str = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
