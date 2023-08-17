ea11ee092abb6637840e72991bca7f59c88f76c6
betiniakarandut <betiniakarandut@gmail.com>
2023-08-17 01:41:09 +0000

    Pycodestyle: fixes

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,14 +6,14 @@ from typing import Any
 
 
 class Cache:
-	"""store an instance of the Redis client as a private variable
-	and flush the instance using flushdb.
-	"""
-	def __init__(self):
-		self._redis = redis.Redis()
-		self._redis.flushdb
+    """store an instance of the Redis client as a private variable
+    and flush the instance using flushdb.
+    """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb
 
-	def store(self, data: Any) -> str:
-		key = str(uuid.uuid4)
-		self._redis.set(key, data)
-		return key
+    def store(self, data: Any) -> str:
+        key = str(uuid.uuid4)
+        self._redis.set(key, data)
+        return key
