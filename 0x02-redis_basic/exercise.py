1112851dd4b3e51cf41254e9006b4fe72fa907db
betiniakarandut <betiniakarandut@gmail.com>
2023-08-17 05:04:43 +0100

    Replay function: Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,47 @@
 """Module for exercise.py"""
 import redis
 import uuid
+from functools import wraps
 from typing import Any, Union, Callable
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator function that takes in a callable
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """increment count"""
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator function to store the history of inputs
+      and outputs for a function
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Store inputs and outputs in Redis lists"""
+        if isinstance(self._redis, redis.Redis):
+            inputs_key = f"{method.__qualname__}:inputs"
+            outputs_key = f"{method.__qualname__}:outputs"
+
+            # Store input arguments
+            self._redis.rpush(inputs_key, str(args))
+
+            # Call the original method and get the output
+            result = method(self, *args, **kwargs)
+
+            # Store the output
+            self._redis.rpush(outputs_key, str(result))
+
+            return result
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
