8f950f6ecb8ad307fe4f7a5731801ca0ab8f68a7
betiniakarandut <betiniakarandut@gmail.com>
2023-08-17 02:22:46 +0100

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+"""Module for exercise.py"""
+import redis
+import uuid
+from typing import Any
 
+
+class Cache:
+	"""store an instance of the Redis client as a private variable
+	and flush the instance using flushdb.
+	"""
+	def __init__(self):
+		self._redis = redis.Redis()
+		self._redis.flushdb
+
+	def store(self, data: Any) -> str:
+		key = str(uuid.uuid4)
+		self._redis.set(key, data)
+		return key
