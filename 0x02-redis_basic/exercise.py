e2ce1847bb0ec36131487da35b9bf8f7c5c7b388
betiniakarandut <betiniakarandut@gmail.com>
2023-08-17 05:43:42 +0100

    Replay Introduce: Decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -53,6 +53,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key: str = str(uuid.uuid1())
         self._redis.set(key, data)
